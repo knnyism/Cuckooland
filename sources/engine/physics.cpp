7969c8bf327731b71ac24853502692eb373cbdda
kennyjeopardy <icanismar@gmail.com>
2024-02-14 00:25:15 +0300

    feat: physics

---
+++ sources/engine/physics.cpp
@@ -0,0 +1,52 @@
+#include <engine/physics.h>
+
+bool ObjectLayerPairFilterImpl::ShouldCollide(ObjectLayer inObject1, ObjectLayer inObject2) const
+{
+	switch (inObject1)
+	{
+	case Layers::NON_MOVING:
+		return inObject2 == Layers::MOVING; // Non moving only collides with moving
+	case Layers::MOVING:
+		return true; // Moving collides with everything
+	default:
+		JPH_ASSERT(false);
+		return false;
+	}
+}
+
+uint BPLayerInterfaceImpl::GetNumBroadPhaseLayers() const
+{
+	return BroadPhaseLayers::NUM_LAYERS;
+}
+
+BroadPhaseLayer BPLayerInterfaceImpl::GetBroadPhaseLayer(ObjectLayer inLayer) const
+{
+	JPH_ASSERT(inLayer < Layers::NUM_LAYERS);
+	return mObjectToBroadPhase[inLayer];
+}
+
+#if defined(JPH_EXTERNAL_PROFILE) || defined(JPH_PROFILE_ENABLED)
+const char* BPLayerInterfaceImpl::GetBroadPhaseLayerName(BroadPhaseLayer inLayer) const
+{
+	switch ((BroadPhaseLayer::Type)inLayer)
+	{
+	case (BroadPhaseLayer::Type)BroadPhaseLayers::NON_MOVING:	return "NON_MOVING";
+	case (BroadPhaseLayer::Type)BroadPhaseLayers::MOVING:		return "MOVING";
+	default:													JPH_ASSERT(false); return "INVALID";
+	}
+}
+#endif // JPH_EXTERNAL_PROFILE || JPH_PROFILE_ENABLED
+
+bool ObjectVsBroadPhaseLayerFilterImpl::ShouldCollide(ObjectLayer inLayer1, BroadPhaseLayer inLayer2) const
+{
+	switch (inLayer1)
+	{
+	case Layers::NON_MOVING:
+		return inLayer2 == BroadPhaseLayers::MOVING;
+	case Layers::MOVING:
+		return true;
+	default:
+		JPH_ASSERT(false);
+		return false;
+	}
+}
