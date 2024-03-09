9be4a64f7f25ea422c4538a8c9bcafc8c027cb4c
kennyjeopardy <icanismar@gmail.com>
2024-03-09 04:01:04 +0300

    feat: pbr lighting system

--- sources/lighting.cpp
+++ sources/lighting.cpp
@@ -1,39 +1,93 @@
 #include <lighting.h>
+#include <iostream>
+#include <resources.h>
 
+raylib::Texture albedoDefault, aoDefault, metallicDefault, normalDefault, roughnessDefault;
+
+static int lightCount = 0;
+static raylib::Shader* shader;
 
 void LoadLighting() {
     shader = new raylib::Shader(
-        TextFormat("resources/shaders/glsl%i/pbr.vs", GLSL_VERSION),
-        TextFormat("resources/shaders/glsl%i/pbr.fs", GLSL_VERSION)
+        GetAssetPath("/shaders/pbr.vs"),
+        GetAssetPath("/shaders/pbr.fs")
     );
 
-    // Load PBR shader and setup all required locations
     shader->locs[SHADER_LOC_MAP_ALBEDO] = shader->GetLocation("albedoMap");
-    shader->locs[SHADER_LOC_MAP_METALNESS] = shader->GetLocation("mraMap");
     shader->locs[SHADER_LOC_MAP_NORMAL] = shader->GetLocation("normalMap");
+    shader->locs[SHADER_LOC_MAP_METALNESS] = shader->GetLocation("metallicMap");
+    shader->locs[SHADER_LOC_MAP_ROUGHNESS] = shader->GetLocation("roughnessMap");
+    shader->locs[SHADER_LOC_MAP_OCCLUSION] = shader->GetLocation("aoMap");
 
-    shader->locs[SHADER_LOC_MAP_EMISSION] = shader->GetLocation("emissiveMap");
-    shader->locs[SHADER_LOC_COLOR_DIFFUSE] = shader->GetLocation("albedoColor");
+    shader->locs[SHADER_LOC_MATRIX_VIEW] = shader->GetLocation("matView");
+    shader->locs[SHADER_LOC_MATRIX_PROJECTION] = shader->GetLocation("matProjection");
+    shader->locs[SHADER_LOC_MATRIX_MODEL] = shader->GetLocation("matModel");
+    shader->locs[SHADER_LOC_MATRIX_MVP] = shader->GetLocation("mvp");
+    shader->locs[SHADER_LOC_VECTOR_VIEW] = shader->GetLocation("camPos");
 
-    // Setup additional required shader locations, including lights data
-    shader->locs[SHADER_LOC_VECTOR_VIEW] = shader->GetLocation("viewPos");
-
-    int lightsCountLoc = shader->GetLocation("numOfLights");
-    int maxLightCount = MAX_LIGHTS;
-    shader->SetValue(lightsCountLoc, &maxLightCount, SHADER_UNIFORM_INT);
-
-    float ambientIntensity = 0.02f;
-    raylib::Color ambientColor(26, 32, 135, 255);
-    Vector3 ambientColorNormalized = { (float)ambientColor.r / 255.0f, (float)ambientColor.g / 255.0f, (float)ambientColor.b / 255.0f };
-
-    shader->SetValue(shader->GetLocation("ambientColor"), &ambientColorNormalized, SHADER_UNIFORM_VEC3);
-    shader->SetValue(shader->GetLocation("ambient"), &ambientIntensity, SHADER_UNIFORM_FLOAT);
-
-    Vector2 floorTextureTiling{ 0.5f, 0.5f };
-    shader->SetValue(shader->GetLocation("tiling"), &floorTextureTiling, SHADER_UNIFORM_VEC2);
+    albedoDefault = LoadTextureFromImage(GenImageColor(1, 1, raylib::WHITE));
+    aoDefault = LoadTextureFromImage(GenImageColor(1, 1, raylib::WHITE));
+    metallicDefault = LoadTextureFromImage(GenImageColor(1, 1, raylib::BLACK));
+    normalDefault = LoadTextureFromImage(GenImageColor(1, 1, { 128, 128, 255, 255 }));
+    roughnessDefault = LoadTextureFromImage(GenImageColor(1, 1, raylib::WHITE));
 }
 
-Light CreateLight(int type, Vector3 position, Vector3 target, raylib::Color color, float intensity) {
-    Light light{ 0 };
+void UpdateLighting() {
+    float cameraPos[3] = { camera->position.x, camera->position.y, camera->position.z };
+    shader->SetValue(shader->locs[SHADER_LOC_VECTOR_VIEW], cameraPos, SHADER_UNIFORM_VEC3);
+}
+
+static void UpdateLight(Light light) {
+    float color[3] = { light.color.r, light.color.g, light.color.b };
+
+    shader->SetValue(light.positionLoc, &light.pos, SHADER_UNIFORM_VEC3);
+    shader->SetValue(light.colorLoc, color, SHADER_UNIFORM_VEC3);
+    shader->SetValue(light.targetLoc, &light.target, SHADER_UNIFORM_VEC3);
+    shader->SetValue(light.intensityLoc, &light.intensity, SHADER_UNIFORM_FLOAT);
+    shader->SetValue(light.typeLoc, &light.type, SHADER_UNIFORM_INT);
+    shader->SetValue(light.enabledLoc, &light.enabled, SHADER_UNIFORM_INT);
+}
+
+Light CreateLight(LightType type, Vector3 position, Vector3 target, raylib::Color color, float intensity) {
+    Light light{};
+
+    light.enabled = true;
+
+    light.type = type;
+    light.pos = position;
+    light.target = target;
+    light.color = color;
+    light.intensity = intensity;
+
+    std::string lightIndex = std::to_string(lightCount);
+
+    light.positionLoc = shader->GetLocation("lights[" + lightIndex + "].pos");
+    light.targetLoc = shader->GetLocation("lights[" + lightIndex + "].target");
+    light.colorLoc = shader->GetLocation("lights[" + lightIndex + "].color");
+    light.intensityLoc = shader->GetLocation("lights[" + lightIndex + "].intensity");
+    light.typeLoc = shader->GetLocation("lights[" + lightIndex + "].type");
+    light.enabledLoc = shader->GetLocation("lights[" + lightIndex + "].on");
+
+    UpdateLight(light);
+
+    lightCount++;
+
     return light;
+}
+
+Material LoadPbrMaterial(Texture albedo, Texture normal, Texture roughness) {
+    Material mat = LoadMaterialDefault();
+    mat.shader = *shader;
+
+    mat.maps[MATERIAL_MAP_ALBEDO].texture = (albedo.id != 0) ? albedo : albedoDefault;
+    mat.maps[MATERIAL_MAP_NORMAL].texture = (normal.id != 0) ? normal : normalDefault;
+    mat.maps[MATERIAL_MAP_METALNESS].texture = (roughness.id != 0) ? roughness : roughnessDefault;
+
+    GenTextureMipmaps(&mat.maps[MATERIAL_MAP_ALBEDO].texture);
+    GenTextureMipmaps(&mat.maps[MATERIAL_MAP_NORMAL].texture);
+    GenTextureMipmaps(&mat.maps[MATERIAL_MAP_METALNESS].texture);
+    GenTextureMipmaps(&mat.maps[MATERIAL_MAP_ROUGHNESS].texture);
+    GenTextureMipmaps(&mat.maps[MATERIAL_MAP_OCCLUSION].texture);
+
+    return mat;
 }
--- sources/lighting.h
+++ sources/lighting.h
@@ -1,29 +1,35 @@
+/*
+* lighting.h
+*
+* To make the game look less eyesore, I wanted to add some lighting.
+* Even though the project will not be assessed on the graphics, I wanted to learn
+* to use GLSL shaders and how to communicate with them.
+*
+* The PBR shader used can be found here:
+* https://github.com/Not-Nik/rlpbr
+*
+*/
+
 #pragma once
 
 #include <globals.h>
-
 #include <raylib-cpp.hpp>
 
-#if defined(PLATFORM_DESKTOP)
-#define GLSL_VERSION            330
-#else   // PLATFORM_ANDROID, PLATFORM_WEB
-#define GLSL_VERSION            120
-#endif
-
-#define MAX_LIGHTS  4           // Max dynamic lights supported by shader
-
 enum LightType {
-    LIGHT_DIRECTIONAL = 0,
-    LIGHT_POINT,
-    LIGHT_SPOT
+    LIGHT_POINT = 1,
+    LIGHT_SPOT = 2,
+    LIGHT_SUN = 3
 };
 
 struct Light {
-    int type;
     int enabled;
-    Vector3 position;
-    Vector3 target;
-    float color[4];
+
+    LightType type;
+
+    raylib::Vector3 pos;
+    raylib::Vector3 target;
+    raylib::Color color;
+
     float intensity;
 
     // Shader light parameters locations
@@ -35,9 +41,7 @@ struct Light {
     int intensityLoc;
 };
 
-inline int lightsCount = 0;
-
-inline raylib::Shader* shader;
-
 void LoadLighting();
-Light CreateLight(int type, Vector3 position, Vector3 target, raylib::Color color, float intensity);
+void UpdateLighting();
+Light CreateLight(LightType type, Vector3 position, Vector3 target, raylib::Color color, float intensity);
+Material LoadPbrMaterial(Texture albedo, Texture normal, Texture roughness);
