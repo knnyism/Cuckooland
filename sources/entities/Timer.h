fbbc31e9c30b4e23e1a4b3dc0be0f541c41a5876
kennyjeopardy <icanismar@gmail.com>
2024-04-05 20:57:13 +0300

    feat: timer entity

---
+++ sources/entities/Timer.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <game.h>
+#include <functional>
+
+class TimerFunc {
+public:
+    f32 time;
+    std::function<void()> func;
+    bool isCalled;
+
+    TimerFunc(f32 time, std::function<void()> func) : time(time), func(func), isCalled(false) {};
+};
+
+class Timer : public Entity {
+public:
+    f32 time;
+    std::vector<TimerFunc> funcs;
+    bool isActive;
+
+    void Timer::Reset() {
+        time = 0.0f;
+    }
+
+    void Timer::SetActive(bool value) {
+        isActive = value;
+    }
+
+    void Timer::AddEvent(f32 time, std::function<void()> func) {
+        funcs.push_back(TimerFunc(time, func));
+    };
+
+    void Timer::Tick() override {
+        if (!isActive) { return; }
+
+        time += TICK_DURATION;
+
+        for (TimerFunc& timerFunc : funcs) {
+            if (timerFunc.isCalled) { continue; }
+
+            if (time > timerFunc.time) {
+                timerFunc.func();
+                timerFunc.isCalled = true;
+            }
+        }
+    }
+};
