21ee8794cfc4f8b23791f97e2349116befaf9ceb
kennyjeopardy <icanismar@gmail.com>
2024-04-05 21:20:58 +0300

    Revert "feat: cuckoo pickup animation"
    This reverts commit 46b7195ab4b6ecd5908e4bfaceaf1dc180dbc3a4.

--- sources/entities/Cuckoo.h
+++ sources/entities/Cuckoo.h
@@ -3,36 +3,25 @@
 #include <game.h>
 #include <entities/Prop.h>
 #include <entities/Player.h>
-#include <entities/Trigger.h>
 
 class Cuckoo : public Entity {
 public:
     Prop* prop;
 
     void Load(Vec3 position, Quat rotation, bool playLongAnimation = false);
-    void SetTriggerFunc(std::function<void(BodyID)> func);
+    void HandlePlayer(Player* player);
 
     void Tick();
     void Render();
-
-    void AfterCamera() override;
 private:
-    void OnTriggerTouched(BodyID bodyId);
-
-    std::function<void(BodyID)> triggerFunc;
-    Trigger* trigger;
-
     bool playLongAnimation;
     raylib::Model* animationModel;
 
     game::Sound cuckooGetSound = game::Sound("cuckoo_get");
-    game::Sound cuckooAddSound = game::Sound("cuckoo_add");
 
-    bool added = false;
     bool acquired = false;
     bool animationFinished = false;
 
-    Mat44 lastTransform;
     Mat44 cameraRelativeMatrix;
 
     f32 acquireTime;
