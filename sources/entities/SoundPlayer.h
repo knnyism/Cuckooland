637996fcbaf363168a198ad607fe44b925c344c2
kennyjeopardy <icanismar@gmail.com>
2024-04-05 21:26:58 +0300

    feat: sound player entity

---
+++ sources/entities/SoundPlayer.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <game.h>
+
+class SoundPlayer : public Entity {
+public:
+    game::Sound3D* sound;
+
+    bool autoRemove;
+
+    void SoundPlayer::Load(game::Sound3D* sound, bool autoRemove) {
+        this->sound = sound;
+        this->autoRemove = autoRemove;
+    };
+
+    void SoundPlayer::Play() {
+        sound->Play();
+    }
+
+    void SoundPlayer::Stop() {
+        sound->Stop();
+    }
+
+    void SoundPlayer::Tick() override {};
+
+    void SoundPlayer::Render() override {
+        if (!sound->isLooped && autoRemove) {
+            DestroyEntity(this);
+        }
+    };
+};
