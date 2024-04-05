7b5ed2f37191ee428299b0fb8800486f9f4b8651
kennyjeopardy <icanismar@gmail.com>
2024-04-05 21:13:00 +0300

    feat: barrier entity

---
+++ sources/entities/Barrier.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <game.h>
+
+class Barrier : public Entity {
+public:
+    void Load(Vec3 pos = Vec3::sZero(), Quat rot = Quat::sIdentity(), Vec3 size = Vec3::sReplicate(1.0f), ObjectLayer layer = Layers::MOVING) {
+        bodyId = body_interface->CreateAndAddBody(BodyCreationSettings(new BoxShape(size / 2), pos, rot, EMotionType::Static, layer), EActivation::Activate);
+    };
+
+    void Destroy() override {
+        body_interface->RemoveBody(bodyId);
+        body_interface->DestroyBody(bodyId);
+    }
+private:
+    BodyID bodyId;
+};
