b371c0c580ecfdeb93d9f325fc8580e485af2b0d
kennyjeopardy <icanismar@gmail.com>
2024-04-05 21:06:46 +0300

    feat: settriggerfunc, save/load state methods

--- sources/entities/Prop.cpp
+++ sources/entities/Prop.cpp
@@ -1,12 +1,32 @@
 #include <entities/Prop.h>
 
-void Prop::Load(const string path, Vec3 position, Quat rotation, f32 overrideMass, f32 overrideFriction, ObjectLayer layer, PropMaterial material) {
-    model.Load(path, position, rotation, overrideMass, overrideFriction, EMotionType::Dynamic, layer);
+void Prop::Load(const string path, Vec3 position, Quat rotation, f32 overrideMass, f32 overrideFriction, ObjectLayer layer, PropMaterial material, EMotionType motionType) {
+    model.Load(path, position, rotation, overrideMass, overrideFriction, motionType, layer);
 
+    modelPath = path;
     hitSound = new game::Sound3D((material == MATERIAL_WOOD) ? "hit_wood" : "hit_metal", Vec3::sZero(), 6, 3);
+
+    spawnMatrix = Mat44::sRotationTranslation(rotation, position);
+    savedMatrix = Mat44::sRotationTranslation(rotation, position);
+}
+
+void Prop::SaveState() {
+    savedMatrix = Mat44::sRotationTranslation(model.body->GetRotation(), model.body->GetPosition());
+}
+
+void Prop::LoadSavedState() {
+    body_interface->SetLinearAndAngularVelocity(model.bodyId, Vec3::sZero(), Vec3::sZero());
+    body_interface->SetPositionAndRotation(model.bodyId, savedMatrix.GetTranslation(), savedMatrix.GetRotation().GetQuaternion(), EActivation::Activate);
+}
+
+void Prop::ResetState() {
+    body_interface->SetLinearAndAngularVelocity(model.bodyId, Vec3::sZero(), Vec3::sZero());
+    body_interface->SetPositionAndRotation(model.bodyId, spawnMatrix.GetTranslation(), spawnMatrix.GetRotation().GetQuaternion(), EActivation::Activate);
 }
 
 void Prop::Destroy() {
+    if (trigger != nullptr) { DestroyEntity(trigger); }
+
     body_interface->RemoveBody(model.bodyId);
     body_interface->DestroyBody(model.bodyId);
 }
@@ -25,7 +45,21 @@ void Prop::Tick() {
     }
 
     model.Tick();
+
+    if (trigger != nullptr) {
+        body_interface->SetPosition(trigger->bodyId, model.body->GetPosition(), EActivation::DontActivate);
+        body_interface->SetRotation(trigger->bodyId, model.body->GetRotation(), EActivation::DontActivate);
+    }
 }
+
+void Prop::SetTriggerFunc(std::function<void(BodyID)> func) {
+    if (trigger == nullptr) {
+        BodyCreationSettings* bcs = new BodyCreationSettings(new ConvexHullShapeSettings(*GetShape(modelPath)), model.body->GetPosition(), model.body->GetRotation(), EMotionType::Dynamic, Layers::TRIGGER);
+
+        trigger = CreateEntity<Trigger>();
+        trigger->Load(bcs, func);
+    }
+};
 
 void Prop::Render() {
     hitSound->position = body_interface->GetPosition(model.bodyId);
--- sources/entities/Prop.h
+++ sources/entities/Prop.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <game.h>
+#include <entities/Trigger.h>
 
 enum PropMaterial {
     MATERIAL_METAL = 0,
@@ -11,14 +12,22 @@ class Prop : public Entity {
 public:
     game::Model model;
 
-    void Load(const string path, Vec3 pos = Vec3::sZero(), Quat rot = Quat::sIdentity(), f32 overrideMass = -1.0f, f32 overrideFriction = 0.5f, ObjectLayer layer = Layers::MOVING, PropMaterial material = MATERIAL_METAL);
+    void Load(const string path, Vec3 pos = Vec3::sZero(), Quat rot = Quat::sIdentity(), f32 overrideMass = -1.0f, f32 overrideFriction = 0.5f, ObjectLayer layer = Layers::MOVING, PropMaterial material = MATERIAL_METAL, EMotionType motionType = EMotionType::Dynamic);
 
     void Tick() override;
     void Render() override;
     void Destroy() override;
 
+    void SaveState();
+    void ResetState();
+    void LoadSavedState();
+    void SetTriggerFunc(std::function<void(BodyID)> func);
+
     void OnContactAdded(const Body& inBody1, const Body& inBody2, const ContactManifold& inManifold, ContactSettings& ioSettings);
 private:
+    string modelPath;
+    Trigger* trigger;
+
     bool emitSound = false;
     game::Sound3D* hitSound;
 
@@ -26,4 +35,7 @@ private:
     BodyID lastHitBody;
     f32 hitVolume = 0.0f;
     f32 lastHitTime = 0.0f;
+
+    Mat44 spawnMatrix;
+    Mat44 savedMatrix;
 };
