bdc6b4289502a709d1e958781b4841f52251c221
kennyjeopardy <icanismar@gmail.com>
2024-04-05 21:30:07 +0300

    feat: use triggers instead of magnitude checks

--- sources/entities/Cuckoo.cpp
+++ sources/entities/Cuckoo.cpp
@@ -6,42 +6,67 @@ void Cuckoo::Load(Vec3 position, Quat rotation, bool playLongAnimation) {
     prop = CreateEntity<Prop>();
     prop->Load("cuckoo", position, rotation, -1.0f, 0.5f, Layers::NO_GRAB);
 
-    if (playLongAnimation)
-        animationModel = GetModel("cuckoo");
+    trigger = CreateEntity<Trigger>();
+    trigger->Load(new BodyCreationSettings(new BoxShape(Vec3::sReplicate(2.0f)), position, rotation, EMotionType::Dynamic, Layers::TRIGGER),
+        [this](BodyID bodyId) {
+            if (triggerFunc != nullptr)
+                triggerFunc(bodyId);
+
+            OnTriggerTouched(bodyId);
+        });
+
+    animationModel = GetModel("cuckoo");
+
+    totalCuckoos++;
 }
 
-void Cuckoo::HandlePlayer(Player* player) {
-    if ((player->moveHelper.position - body_interface->GetPosition(prop->model.bodyId)).Length() < 6.0f) {
-        acquired = true;
-        acquireTime = GetTime();
-
-        cuckooGetSound.Play();
-
-        cameraRelativeMatrix = game::GetCameraMatrix().Inversed() * body_interface->GetWorldTransform(prop->model.bodyId);
-
-        DestroyEntity(prop);
-    }
+void Cuckoo::SetTriggerFunc(std::function<void(BodyID)> func) {
+    triggerFunc = func;
 }
 
-void Cuckoo::Tick() {
-    if (prop->IsAlive) {
-        for (auto& bucket : entities.buckets) {
-            for (Entity* entity : bucket->data) {
-                Player* player = dynamic_cast<Player*>(entity);
+void Cuckoo::OnTriggerTouched(BodyID bodyId) {
+    if (acquired)
+        return;
 
-                if (player) {
-                    HandlePlayer(player);
+    for (auto& bucket : entities.buckets) {
+        for (Entity* entity : bucket->data) {
+            if (!entity->IsAlive)
+                continue;
+
+            Player* player = dynamic_cast<Player*>(entity);
+
+            if (player) {
+                if (bodyId == player->body->GetID()) {
+                    acquired = true;
+                    acquireTime = GetTime();
+
+                    cuckooCount++;
+
+                    if (playLongAnimation)
+                        cuckooGetSound.Play();
+
+                    cameraRelativeMatrix = game::GetCameraMatrix().Inversed() * body_interface->GetWorldTransform(prop->model.bodyId);
+                    lastTransform = body_interface->GetWorldTransform(prop->model.bodyId);
+
+                    DestroyEntity(prop);
+                    DestroyEntity(trigger);
                 }
             }
         }
     }
 }
 
+void Cuckoo::Tick() {
+    if (!acquired) {
+        body_interface->SetPosition(trigger->bodyId, body_interface->GetPosition(prop->model.bodyId), EActivation::DontActivate);
+        body_interface->SetRotation(trigger->bodyId, body_interface->GetRotation(prop->model.bodyId), EActivation::DontActivate);
+    }
+}
+
 void Cuckoo::Render() {
     if (acquired && playLongAnimation) {
-
         Mat44 worldMatrix = game::GetCameraMatrix() * Mat44::sRotationTranslation(cameraRelativeMatrix.GetRotation().GetQuaternion(), cameraRelativeMatrix.GetTranslation() * 0.1f);
-        Mat44 targetMatrix = game::GetCameraMatrix() * Mat44::sTranslation(Vec3(0, -0.05, 0.6));
+        Mat44 targetMatrix = game::GetCameraMatrix() * Mat44::sTranslation(Vec3(0.0f, 0.0f, 0.6f));
 
         f32 t = GetTime() - acquireTime;
         f32 alpha = tween::sineinout(std::clamp(t / 1.0f, 0.0f, 1.0f));
@@ -56,10 +81,69 @@ void Cuckoo::Render() {
         f32 rotationAngle;
         resultMatrix.GetRotation().GetQuaternion().GetAxisAngle(rotationAxis, rotationAngle);
 
+        f32 alpha2 = 1.0f - tween::sineinout(std::clamp((t - 7.0f) / 1.0f, 0.0f, 1.0f));
+        f32 alpha3 = 1.0f - tween::sineinout(std::clamp((t - 7.2f) / 1.2f, 0.0f, 1.0f));
+
+        if (t > 7.0f && !added) {
+            added = true;
+            cuckooAddSound.Play();
+        }
+
         animationModel->Draw(
             { position.GetX(), position.GetY(), position.GetZ() },
             { rotationAxis.GetX(), rotationAxis.GetY(), rotationAxis.GetZ() },
-            rotationAngle * RAD2DEG, Vector3{ 0.1f, 0.1f, 0.1f }
+            rotationAngle * RAD2DEG, Vector3{ 0.1f * alpha3, 0.1f * alpha2, 0.1f * alpha3 }
         );
+
+        if (t > 8.5f) { DestroyEntity(this); }
+    }
+    else if (acquired) {
+        f32 t = GetTime() - acquireTime;
+
+        f32 alpha2 = 1.0f - tween::sineinout(std::clamp(t / 1.0f, 0.0f, 1.0f));
+        f32 alpha3 = 1.0f - tween::sineinout(std::clamp(t / 1.2f, 0.0f, 1.0f));
+
+        Mat44 targetMatrix = game::GetCameraMatrix() * Mat44::sTranslation(Vec3(0, -0.1, 0));
+
+        Quat resultRotation = lastTransform.GetRotation().GetQuaternion().SLERP(targetMatrix.GetRotation().GetQuaternion(), 1.0f - alpha2);
+        Vec3 resultPosition = lastTransform.GetTranslation() + (targetMatrix.GetTranslation() - lastTransform.GetTranslation()) * (1.0f - alpha2);
+
+        Vec3 rotationAxis;
+        f32 rotationAngle;
+        resultRotation.GetAxisAngle(rotationAxis, rotationAngle);
+
+        if (!added) {
+            added = true;
+            cuckooAddSound.Play();
+        }
+
+        animationModel->Draw(
+            { resultPosition.GetX(), resultPosition.GetY(), resultPosition.GetZ() },
+            { rotationAxis.GetX(), rotationAxis.GetY(), rotationAxis.GetZ() },
+            rotationAngle * RAD2DEG, Vector3{ alpha3, alpha2, alpha3 }
+        );
+
+        if (t > 1.2f) { DestroyEntity(this); }
+    }
+}
+
+void Cuckoo::AfterCamera() {
+    if (acquired && playLongAnimation) {
+        f32 t = GetTime() - acquireTime;
+
+        if (t > 1.0f && t < 7.0f) {
+            game::UIObject<raylib::Text> topText(game::UDim2(0.5, 0, 0.4, 0), game::UDim2(0, 0, 0.03, 0), Vector2{ 0.5f,0.5f }, "You got a cuckoo!");
+            topText.object.SetFontSize(topText.GetAbsoluteSize().y);
+            topText.object.SetSpacing(2.0f);
+            topText.size = game::UDim2(0, topText.object.MeasureEx().x, topText.size.y.scale, 0);
+
+            game::UIObject<raylib::Text> bottomText(game::UDim2(0.5, 0, 0.6, 0), game::UDim2(0, 0, 0.03, 0), Vector2{ 0.5f,0.5f }, "Find and collect as many cuckoos as you can!");
+            bottomText.object.SetFontSize(bottomText.GetAbsoluteSize().y);
+            bottomText.object.SetSpacing(2.0f);
+            bottomText.size = game::UDim2(0, bottomText.object.MeasureEx().x, bottomText.size.y.scale, 0);
+
+            topText.object.Draw(topText.GetAbsolutePosition());
+            bottomText.object.Draw(bottomText.GetAbsolutePosition());
+        }
     }
 }
